c5761b116bf6d14716d321876c0275164d126391
Nicholas "LB" Braden <LB@LB-Stuff.com>
2014-08-22 22:01:57 -0500

    Cleanup

--- rebuild.bat
+++
@@ -1,4 +0,0 @@
-del /s /f /q .\build\* && for /f %%f in ('dir /ad /b .\build\') do rd /s /q .\build\%f
-title RecipeSpelunker-Build && mkdir build
-cd build && cmake -G "MinGW Makefiles" -DCMAKE_CXX_FLAGS=" -Wall -Wextra -pedantic -Wno-unused-parameter -Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Wunreachable-code -Wmissing-include-dirs -Wmain" -DCMAKE_BUILD_TYPE=RELEASE  -DSTATIC_BUILD=1 -DSFML_ROOT="C:/MinGW" -DBOOST_ROOT="C:/MinGW" ../
-cmd /k "make && resplunk-client"
